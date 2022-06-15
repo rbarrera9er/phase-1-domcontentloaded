79bf95a35732407838a677dbe0f9eba18a3eef5a
Ryan Barrera <rbarrera9er@gmail.com>
2022-06-14 20:20:41 -0400

    commit tree 87c7ecc39c04c4b1f451af231d708db0f8707da8

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let text = document.querySelector('p')
+  text.innerHTML = 'This is really cool!'
+    //console.log("The DOM has loaded");
+  });
+  
+
+
+  
